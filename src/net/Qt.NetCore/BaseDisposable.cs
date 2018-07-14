5e02f5cd0c6c84efb2bd5a5142520eb26e52b93a
Paul Knopf <pauldotknopf@gmail.com>
2018-07-14 19:52:54 -0400

    Using helper disposable class.

---
+++ src/net/Qt.NetCore/BaseDisposable.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Qt.NetCore
+{
+    public abstract class BaseDisposable : IDisposable
+    {
+        IntPtr _handle;
+        bool _disposed = false;
+        readonly bool _ownesHandle;
+
+        protected BaseDisposable(IntPtr handle, bool ownsHandle = true)
+        {
+            _handle = handle;
+            _ownesHandle = ownsHandle;
+        }
+        
+        ~BaseDisposable()
+        {
+            Dispose(false);
+        }
+
+        public IntPtr Handle
+        {
+            get
+            {
+                if (_disposed)
+                {
+                    throw new ObjectDisposedException($"Type {GetType().Name} is disposed.");
+                }
+                
+                return _handle;
+            }
+        }
+
+        public void Dispose() 
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this); 
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            
+            if (disposing)
+            {
+                DisposeManaged();
+            }
+
+            if (_ownesHandle)
+            {
+                DisposeUnmanaged(_handle);
+            }
+
+            _handle = IntPtr.Zero;
+
+            _disposed = true;
+        }
+
+        protected virtual void DisposeManaged()
+        {
+            
+        }
+
+        protected abstract void DisposeUnmanaged(IntPtr ptr);
+    }
+}
