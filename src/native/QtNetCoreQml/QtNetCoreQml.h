0b7f55fc055bcf0d0667ce205731ca3df820b43d
Paul Knopf <pauldotknopf@gmail.com>
2018-07-14 16:31:39 -0400

    Can check if type is valid.

--- src/native/QtNetCoreQml/QtNetCoreQml.h
+++ src/native/QtNetCoreQml/QtNetCoreQml.h
@@ -17,4 +17,46 @@ enum NetVariantTypeEnum {
 
 #define NetGCHandle void
 
+#if _MSC_VER
+    #include <Windows.h>
+    #include <stdint.h>
+    #include <Tchar.h>
+
+    typedef const wchar_t* BCSTR;
+    typedef const char* LPCSTR;
+    typedef const wchar_t* LPWCSTR;
+
+    #if UNICODE
+        #define LPTSTR(value) L ##value;
+        typedef LPWCSTR LPTCSTR;
+    #else
+        #define LPTSTR(value) value;
+        typedef LPCSTR LPTCSTR;
+    #endif
+#endif
+
+#if !_MSC_VER
+    #define __declspec(dllexport)
+    #define __stdcall
+
+    typedef char16_t* BSTR;
+    typedef const char16_t* BCSTR;
+
+    typedef char* LPSTR;
+    typedef const char* LPCSTR;
+
+    typedef char16_t* LPWSTR;
+    typedef const char16_t* LPWCSTR;
+
+    #if UNICODE
+        #define LPTSTR(value) (LPTSTR)u ##value;
+        typedef LPWSTR LPTSTR;
+        typedef LPWCSTR LPTCSTR;
+    #else
+        #define LPTSTR(value) value;
+        typedef LPSTR LPTSTR;
+        typedef LPCSTR LPTCSTR;
+    #endif
+#endif
+
 #endif // QTNETCOREQML_GLOBAL_H
