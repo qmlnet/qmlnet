6f4f96d80b77bc8fca2f36816ff8f21c14225069
Paul Knopf <pauldotknopf@gmail.com>
2018-07-14 17:14:25 -0400

    Working on the NetTypeInfo pinvoke.

--- src/native/QtNetCoreQml/QtNetCoreQml/types/net_type_info.cpp
+++ src/native/QtNetCoreQml/QtNetCoreQml/types/net_type_info.cpp
@@ -1,76 +1,30 @@
-#include "net_type_info.h"
-#include <QDebug>
-
 #include <QtNetCoreQml/types/net_type_info.h>
 
-NetTypeInfo::NetTypeInfo(std::string fullTypeName) :
-    prefVariantType(NetVariantTypeEnum_Invalid),
-    fullTypeName(fullTypeName)
-{
-}
-
-NetTypeInfo::~NetTypeInfo()
-{
+NetTypeInfo::NetTypeInfo(QString fullTypeName) :
+    _fullTypeName(fullTypeName) {
 
 }
 
-NetVariantTypeEnum NetTypeInfo::GetPrefVariantType()
-{
-    return prefVariantType;
+
+NetTypeInfo::~NetTypeInfo() {
+
 }
 
-void NetTypeInfo::SetPrefVariantType(NetVariantTypeEnum value)
-{
-    prefVariantType = value;
+QString NetTypeInfo::getFullTypeName() {
+    return _fullTypeName;
 }
 
-std::string NetTypeInfo::GetFullTypeName()
-{
-    return fullTypeName;
+NetTypeInfoContainer* type_info_create(LPWSTR fullTypeName) {
+    NetTypeInfoContainer* result = new NetTypeInfoContainer();
+    result->netTypeInfo = QSharedPointer<NetTypeInfo>(new NetTypeInfo(QString::fromUtf16(fullTypeName)));
+    return result;
 }
 
-void NetTypeInfo::SetClassName(std::string className)
-{
-    this->className = className;
+void type_info_destroy(NetTypeInfoContainer* netTypeInfo) {
+    delete netTypeInfo;
+    netTypeInfo = NULL;
 }
 
-std::string NetTypeInfo::GetClassName()
-{
-    return className;
-}
-
-void NetTypeInfo::AddMethod(NetMethodInfo* methodInfo)
-{
-    methods.append(methodInfo);
-}
-
-int NetTypeInfo::GetMethodCount()
-{
-    return methods.length();
-}
-
-NetMethodInfo* NetTypeInfo::GetMethod(int index)
-{
-    if(index < 0) return NULL;
-    if(index >= methods.length()) return NULL;
-
-    return methods.at(index);
-}
-
-void NetTypeInfo::AddProperty(NetPropertyInfo* propertyInfo)
-{
-    properties.append(propertyInfo);
-}
-
-int NetTypeInfo::GetPropertyCount()
-{
-    return properties.length();
-}
-
-NetPropertyInfo* NetTypeInfo::GetProperty(int index)
-{
-    if(index < 0) return NULL;
-    if(index >= properties.length()) return NULL;
-
-    return properties.at(index);
+LPWSTR type_info_getFullTypeName(NetTypeInfoContainer* netTypeInfo) {
+    return (LPWSTR)netTypeInfo->netTypeInfo->getFullTypeName().utf16();
 }
---
+++ src/net/Qt.NetCore.Tests/Types/NetTypeInfoTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Qt.NetCore.Tests.Types
+{
+    public class NetTypeInfoTests
+    {
+        [Fact]
+        public void Can_create_type()
+        {
+            var typeInfo = new NetTypeInfo("fullTypeName");
+
+            typeInfo.FullTypeName.Should().Be("fullTypeName");
+        }
+    }
+}
