453e546e29d150fa87b336811ce65e47bb4657c7
Paul Knopf <pauldotknopf@gmail.com>
2018-07-14 23:18:49 -0400

    Added support for getting method parameters.

--- src/net/Qt.NetCore.Tests/Types/NetTypeManagerTests.cs
+++ src/net/Qt.NetCore.Tests/Types/NetTypeManagerTests.cs
@@ -7,29 +7,57 @@ namespace Qt.NetCore.Tests.Types
 {
     public class NetTypeManagerTests : BaseTests
     {
-        class TestType
+        public class TestType1
         {
-            public void TestMethodNoReturn()
+            public void TestMethod()
             {
                 
-            }
-
-            public int TestMethodReturn()
-            {
-                return 0;
             }
         }
         
         [Fact]
         public void Can_get_net_type()
         {
-            var typeInfo = NetTypeManager.GetTypeInfo<TestType>();
+            var typeInfo = NetTypeManager.GetTypeInfo<TestType1>();
 
-            typeInfo.FullTypeName.Should().Be(typeof(TestType).AssemblyQualifiedName);
-            typeInfo.ClassName.Should().Be("TestType");
+            typeInfo.FullTypeName.Should().Be(typeof(TestType1).AssemblyQualifiedName);
+            typeInfo.ClassName.Should().Be("TestType1");
             typeInfo.PrefVariantType.Should().Be(NetVariantType.Object);
+            typeInfo.MethodCount.Should().Be(1);
+        }
 
-            typeInfo.MethodCount.Should().Be(2);
+        public class TestType2
+        {
+            public void TestMethod(int parameter1, TestType1 parameter2)
+            {
+                
+            }
+        }
+        
+        [Fact]
+        public void Can_get_method_parameters()
+        {
+            var typeInfo = NetTypeManager.GetTypeInfo<TestType2>();
+
+            typeInfo.MethodCount.Should().Be(1);
+            
+            var method = typeInfo.GetMethod(0);
+            method.Should().NotBeNull();
+            method.ParameterCount.Should().Be(2);
+            
+            var parameter1 = method.GetParameter(0);
+            parameter1.Should().NotBeNull();
+            parameter1.Name.Should().Be("parameter1");
+            var parameter1Type = parameter1.Type;
+            parameter1Type.Should().NotBeNull();
+            parameter1Type.ClassName.Should().Be("Int32");
+            
+            var parameter2 = method.GetParameter(1);
+            parameter2.Should().NotBeNull();
+            parameter2.Name.Should().Be("parameter2");
+            var parameter2Type = parameter2.Type;
+            parameter2Type.Should().NotBeNull();
+            parameter2Type.ClassName.Should().Be("TestType1");
         }
 
         [Fact]
