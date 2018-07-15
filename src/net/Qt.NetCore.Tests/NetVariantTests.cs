cc8f46b05d876603f4f5725916f0941da729be3a
Paul Knopf <pauldotknopf@gmail.com>
2018-07-15 02:07:31 -0400

    Minor changes.

---
+++ src/net/Qt.NetCore.Tests/NetVariantTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Qt.NetCore.Tests
+{
+    public class NetVariantTests : BaseTests
+    {
+        [Fact]
+        public void Test()
+        {
+            
+        }
+    }
+}
