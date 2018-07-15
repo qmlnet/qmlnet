a1d60f5f044be3b5bf955229a3fc640cfa44a08c
Paul Knopf <pauldotknopf@gmail.com>
2018-07-15 15:52:42 -0400

    Added the rest of the methods to the variant list.

--- src/native/QtNetCoreQml/QtNetCoreQml/qml/NetVariantList.cpp
+++ src/native/QtNetCoreQml/QtNetCoreQml/qml/NetVariantList.cpp
@@ -14,6 +14,24 @@ int NetVariantList::count() {
     return variants.size();
 }
 
+void NetVariantList::add(QSharedPointer<NetVariant> variant) {
+    variants.append(variant);
+}
+
+QSharedPointer<NetVariant> NetVariantList::get(int index) {
+    if(index < 0) return QSharedPointer<NetVariant>(NULL);
+    if(index >= variants.length()) return QSharedPointer<NetVariant>(NULL);
+    return variants.at(index);
+}
+
+void NetVariantList::remove(int index) {
+    variants.removeAt(index);;
+}
+
+void NetVariantList::clear() {
+    variants.clear();
+}
+
 extern "C" {
 
 NetVariantListContainer* net_variant_list_create() {
@@ -28,6 +46,26 @@ void net_variant_list_destroy(NetVariantListContainer* container) {
 
 int net_variant_list_count(NetVariantListContainer* container) {
     return container->list->count();
+}
+
+void net_variant_list_add(NetVariantListContainer* container, NetVariantContainer* variant) {
+    container->list->add(variant->variant);
+}
+
+NetVariantContainer* net_variant_list_get(NetVariantListContainer* container, int index){
+    QSharedPointer<NetVariant> variant = container->list->get(index);
+    if(variant == NULL) return NULL;
+    NetVariantContainer* result = new NetVariantContainer();
+    result->variant = variant;
+    return result;
+}
+
+void net_variant_list_remove(NetVariantListContainer* container, int index) {
+    container->list->remove(index);
+}
+
+void net_variant_list_clear(NetVariantListContainer* container) {
+    container->list->clear();
 }
 
 }
--- src/net/Qt.NetCore/Qml/NetVariantList.cs
+++ src/net/Qt.NetCore/Qml/NetVariantList.cs
@@ -20,6 +20,28 @@ namespace Qt.NetCore.Qml
 
         public int Count => Interop.NetVariantList.Count(Handle);
 
+        public void Add(NetVariant variant)
+        {
+            Interop.NetVariantList.Add(Handle, variant.Handle);
+        }
+
+        public NetVariant Get(int index)
+        {
+            var result = Interop.NetVariantList.Get(Handle, index);
+            if (result == IntPtr.Zero) return null;
+            return new NetVariant(result);
+        }
+
+        public void Remove(int index)
+        {
+            Interop.NetVariantList.Remove(Handle, index);
+        }
+
+        public void Clear()
+        {
+            Interop.NetVariantList.Clear(Handle);
+        }
+        
         protected override void DisposeUnmanaged(IntPtr ptr)
         {
             Interop.NetVariantList.Destroy(ptr);
@@ -35,5 +57,14 @@ namespace Qt.NetCore.Qml
 
         [NativeSymbol(Entrypoint = "net_variant_list_count")]
         int Count(IntPtr list);
+
+        [NativeSymbol(Entrypoint = "net_variant_list_add")]
+        void Add(IntPtr list, IntPtr variant);
+        [NativeSymbol(Entrypoint = "net_variant_list_get")]
+        IntPtr Get(IntPtr list, int index);
+        [NativeSymbol(Entrypoint = "net_variant_list_remove")]
+        void Remove(IntPtr list, int index);
+        [NativeSymbol(Entrypoint = "net_variant_list_clear")]
+        void Clear(IntPtr list);
     }
 }
