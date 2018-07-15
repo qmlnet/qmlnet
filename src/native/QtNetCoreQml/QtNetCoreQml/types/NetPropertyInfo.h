6219cd206d9ec73e799587b2f738e1df2d49a2d0
Paul Knopf <pauldotknopf@gmail.com>
2018-07-14 23:56:28 -0400

    Renaming.

---
+++ src/native/QtNetCoreQml/QtNetCoreQml/types/NetPropertyInfo.h
@@ -0,0 +1,27 @@
+#ifndef NET_TYPE_INFO_PROPERTY_H
+#define NET_TYPE_INFO_PROPERTY_H
+
+#include <QtNetCoreQml/types/NetTypeInfo.h>
+
+class NetPropertyInfo {
+public:
+    NetPropertyInfo(QSharedPointer<NetTypeInfo> parentType,
+                    QString name,
+                    QSharedPointer<NetTypeInfo> returnType,
+                    bool canRead,
+                    bool canWrite);
+    QSharedPointer<NetTypeInfo> getParentType();
+    QString getPropertyName();
+    QSharedPointer<NetTypeInfo> getReturnType();
+    bool canRead();
+    bool canWrite();
+private:
+    QSharedPointer<NetTypeInfo> _parentType;
+    QString _name;
+    QSharedPointer<NetTypeInfo> _returnType;
+    bool _canRead;
+    bool _canWrite;
+};
+
+
+#endif // NET_TYPE_INFO_PROPERTY_H
