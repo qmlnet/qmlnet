e939171f51958d79266648d1a344a142f16d7957
Paul Knopf <pauldotknopf@gmail.com>
2018-07-14 23:40:54 -0400

    Added support for getting method name and return type.

--- src/net/Qt.NetCore.Tests/Types/NetTypeManagerTests.cs
+++ src/net/Qt.NetCore.Tests/Types/NetTypeManagerTests.cs
@@ -60,6 +60,55 @@ namespace Qt.NetCore.Tests.Types
             parameter2Type.ClassName.Should().Be("TestType1");
         }
 
+        public class TestType3
+        {
+            public TestType3 TestMethod()
+            {
+                return null;
+            }
+        }
+
+        [Fact]
+        public void Can_get_method_with_return_type()
+        {
+            var type = NetTypeManager.GetTypeInfo<TestType3>();
+            var returnType = type.GetMethod(0).ReturnType;
+            returnType.Should().NotBeNull();
+            returnType.ClassName.Should().Be("TestType3");
+        }
+
+        public class TestType4
+        {
+            public void TestMethod()
+            {
+                
+            }
+        }
+        
+        [Fact]
+        public void Can_get_method_without_return_type()
+        {
+            var type = NetTypeManager.GetTypeInfo<TestType4>();
+            var returnType = type.GetMethod(0).ReturnType;
+            returnType.Should().BeNull();
+        }
+
+        public class TestType5
+        {
+            public void ThisIsMethodName()
+            {
+                
+            }
+        }
+
+        [Fact]
+        public void Can_get_method_name()
+        {
+            var type = NetTypeManager.GetTypeInfo<TestType5>();
+            var method = type.GetMethod(0);
+            method.MethodName.Should().Be("ThisIsMethodName");
+        }
+
         [Fact]
         public void Null_type_returned_for_invalid_type()
         {
