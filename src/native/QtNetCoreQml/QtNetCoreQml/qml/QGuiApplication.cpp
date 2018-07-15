f225f99083b904a340302d8471edef576f5d5152
Paul Knopf <pauldotknopf@gmail.com>
2018-07-15 18:20:40 -0400

    Added back in thread dispatching.

--- src/native/QtNetCoreQml/QtNetCoreQml/qml/QGuiApplication.cpp
+++ src/native/QtNetCoreQml/QtNetCoreQml/qml/QGuiApplication.cpp
@@ -1,12 +1,24 @@
 #include <QtNetCoreQml/qml/QGuiApplication.h>
 #include <QGuiApplication>
 
+GuiThreadContextTriggerCallback::GuiThreadContextTriggerCallback() :
+    callback(NULL) {
+
+}
+
+void GuiThreadContextTriggerCallback::trigger() {
+    if (callback) {
+       callback();
+    }
+}
+
 extern "C" {
 
 QGuiApplicationContainer* qguiapplication_create() {
     QGuiApplicationContainer* result = new QGuiApplicationContainer();
     result->argCount = 0;
     result->guiApp = QSharedPointer<QGuiApplication>(new QGuiApplication(result->argCount, (char**)NULL, 0));
+    result->callback = QSharedPointer<GuiThreadContextTriggerCallback>(new GuiThreadContextTriggerCallback());
     return result;
 }
 
@@ -16,6 +28,14 @@ void qguiapplication_destroy(QGuiApplicationContainer* container) {
 
 int qguiapplication_exec(QGuiApplicationContainer* container) {
     return container->guiApp->exec();
+}
+
+void qguiapplication_addTriggerCallback(QGuiApplicationContainer* container, guiThreadTriggerCb callback) {
+    container->callback->callback = callback;
+}
+
+void qguiapplication_requestTrigger(QGuiApplicationContainer* container) {
+    QMetaObject::invokeMethod(container->callback.data(), "trigger", Qt::QueuedConnection);
 }
 
 }
--- src/native/QtNetCoreQml/QtNetCoreQml/qml/QGuiApplication.h
+++ src/native/QtNetCoreQml/QtNetCoreQml/qml/QGuiApplication.h
@@ -4,9 +4,21 @@
 #include <QGuiApplication>
 #include <QSharedPointer>
 
+typedef void (*guiThreadTriggerCb)();
+
+class GuiThreadContextTriggerCallback : public QObject {
+    Q_OBJECT
+public:
+    GuiThreadContextTriggerCallback();
+    guiThreadTriggerCb callback;
+public slots:
+    void trigger();
+};
+
 struct QGuiApplicationContainer {
     int argCount;
     QSharedPointer<QGuiApplication> guiApp;
+    QSharedPointer<GuiThreadContextTriggerCallback> callback;
 };
 
 #endif // NET_QGUIAPPLICATION_H
