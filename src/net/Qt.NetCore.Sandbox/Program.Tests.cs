2a6789b97468ca8c9c4806a4b5653776903d77c0
Paul Knopf <pauldotknopf@gmail.com>
2018-07-17 23:45:27 -0400

    Added helper code to run tests in sandbox. This allows me to test with Qt Creator easily.
    Set QT_QPA_PLATFORM=offscreen when running the tests. It gives a bunch of warnings (harmless) otherwise.

--- src/net/Qt.NetCore.Sandbox/Program.Tests.cs
+++ src/net/Qt.NetCore.Sandbox/Program.Tests.cs
@@ -1,19 +1,72 @@
-﻿using System;
-using Qt.NetCore.Qml;
-using Qt.NetCore.Types;
+﻿using System.Collections.Generic;
+using System.Threading;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace Qt.NetCore.Sandbox
 {
-    public class Program
+    class Sink : IMessageSink
     {
-        public class TestType
+        public bool OnMessage(IMessageSinkMessage message)
+        {
+            switch (message)
+            {
+                case Xunit.Sdk.TestAssemblyFinished _:
+                    TestAssemblyFinished.Set();
+                    break;
+            }
+            return true;
+        }
+        
+        public readonly ManualResetEvent TestAssemblyFinished = new ManualResetEvent(false);
+    }
+
+    class SinkWithTypes : IMessageSinkWithTypes
+    {
+        public void Dispose()
         {
             
         }
-        
+
+        public bool OnMessageWithTypes(IMessageSinkMessage message, HashSet<string> messageTypes)
+        {
+            switch (message)
+            {
+                case DiscoveryCompleteMessage _:
+                    DiscoveryComplete.Set();
+                    break;
+                case Xunit.Sdk.TestCaseDiscoveryMessage testCaseDiscoveryMessage:
+                    TestCases.Add(testCaseDiscoveryMessage.TestCase);
+                    break;
+            }
+
+            return true;
+        }
+
+        public readonly ManualResetEvent DiscoveryComplete = new ManualResetEvent(false);
+
+        public readonly List<ITestCase> TestCases = new List<ITestCase>();
+    }
+    
+    class Program
+    {
         static void Main()
         {
+            var config = ConfigReader.Load(typeof(Tests.BaseTests).Assembly.Location);
+            var controller = new XunitFrontController(AppDomainSupport.Denied, typeof(Tests.BaseTests).Assembly.Location);
+            var discoverOptions = TestFrameworkOptions.ForDiscovery(config);
+            var executionOptions = TestFrameworkOptions.ForExecution(config);
             
+            var sinkWithTypes = new SinkWithTypes();
+            var sink = new Sink();
+            
+            // Discover the tests
+            controller.Find(false, sinkWithTypes, discoverOptions);
+            sinkWithTypes.DiscoveryComplete.WaitOne();
+            
+            // Run the tests
+            controller.RunTests(sinkWithTypes.TestCases, sink, executionOptions);
+            sink.TestAssemblyFinished.WaitOne();
         }
     }
 }
