d705856008fbece1f4c0b72cede60ccfcd40a1a5
Paul Knopf <pauldotknopf@gmail.com>
2018-07-26 16:23:26 -0400

    Support for invoking the callbacks.

--- src/net/Qml.Net.Tests/Qml/JsValueTests.cs
+++ src/net/Qml.Net.Tests/Qml/JsValueTests.cs
@@ -11,18 +11,28 @@ namespace Qml.Net.Tests.Qml
     {
         public class JsTestsQml
         {
-            public virtual void Method(NetJsValue value)
+            public virtual void Method(INetJsValue value)
             {
 
+            }
+
+            public virtual void MethodWithoutParams()
+            {
+                
+            }
+            
+            public virtual void MethodWithParameters(string param1, int param2)
+            {
+                
             }
         }
 
         [Fact]
         public void Can_send_function()
         {
-            NetJsValue jsValue = null;
-            Mock.Setup(x => x.Method(It.IsAny<NetJsValue>()))
-                .Callback(new Action<NetJsValue>(x => jsValue = x));
+            INetJsValue jsValue = null;
+            Mock.Setup(x => x.Method(It.IsAny<INetJsValue>()))
+                .Callback(new Action<INetJsValue>(x => jsValue = x));
             
             NetTestHelper.RunQml(qmlApplicationEngine,
                 @"
@@ -36,7 +46,7 @@ namespace Qml.Net.Tests.Qml
                     }
                 ");
 
-            Mock.Verify(x => x.Method(It.IsAny<NetJsValue>()), Times.Once);
+            Mock.Verify(x => x.Method(It.IsAny<INetJsValue>()), Times.Once);
             jsValue.Should().NotBeNull();
             jsValue.IsCallable.Should().BeTrue();
         }
@@ -44,9 +54,9 @@ namespace Qml.Net.Tests.Qml
         [Fact]
         public void Can_send_non_function()
         {
-            NetJsValue jsValue = null;
-            Mock.Setup(x => x.Method(It.IsAny<NetJsValue>()))
-                .Callback(new Action<NetJsValue>(x => jsValue = x));
+            INetJsValue jsValue = null;
+            Mock.Setup(x => x.Method(It.IsAny<INetJsValue>()))
+                .Callback(new Action<INetJsValue>(x => jsValue = x));
             
             NetTestHelper.RunQml(qmlApplicationEngine,
                 @"
@@ -60,9 +70,37 @@ namespace Qml.Net.Tests.Qml
                     }
                 ");
 
-            Mock.Verify(x => x.Method(It.IsAny<NetJsValue>()), Times.Once);
+            Mock.Verify(x => x.Method(It.IsAny<INetJsValue>()), Times.Once);
             jsValue.Should().NotBeNull();
             jsValue.IsCallable.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Can_invoke_js_callback()
+        {
+            Mock.Setup(x => x.Method(It.IsAny<INetJsValue>()))
+                .Callback(new Action<INetJsValue>(x =>
+                {
+                    x.Call();
+                }));
+            Mock.Setup(x => x.MethodWithoutParams());
+            
+            NetTestHelper.RunQml(qmlApplicationEngine,
+                @"
+                    import QtQuick 2.0
+                    import tests 1.0
+                    JsTestsQml {
+                        id: test
+                        Component.onCompleted: function() {
+                            test.Method(function() {
+                                test.MethodWithoutParams()
+                            })
+                        }
+                    }
+                ");
+
+            Mock.Verify(x => x.Method(It.IsAny<INetJsValue>()), Times.Once);
+            Mock.Verify(x => x.MethodWithoutParams(), Times.Once);
         }
     }
 }
