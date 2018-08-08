9c1dfb8db94f695c0f2f727df47cb0f0104e9eb0
Paul Knopf <pauldotknopf@gmail.com>
2018-08-08 03:28:24 -0400

    Forgot this.

--- build/travis.sh
+++ build/travis.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Before we run the build, get gitversion and generate a version.json.
 nuget install GitVersion.CommandLine -Version 4.0.0-beta0012
-mono ./GitVersion.CommandLine.3.6.5/tools/GitVersion.exe > version.json
+mono ./GitVersion.CommandLine.4.0.0-beta0012/tools/GitVersion.exe > version.json
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 
