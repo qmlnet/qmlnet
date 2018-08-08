e0822f9b392bd265f0e7858b6b2627f22ddbcd92
Paul Knopf <pauldotknopf@gmail.com>
2018-08-08 03:22:30 -0400

    Using consistent gitversion versions.

--- build/travis.sh
+++ build/travis.sh
@@ -4,7 +4,7 @@ set -e
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Before we run the build, get gitversion and generate a version.json.
-nuget install GitVersion.CommandLine -Version 3.6.5
+nuget install GitVersion.CommandLine -Version 4.0.0-beta0012
 mono ./GitVersion.CommandLine.3.6.5/tools/GitVersion.exe > version.json
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
