7997adbdc7fc25d27298bfbac546596c36b1f796
Paul Knopf <pauldotknopf@gmail.com>
2018-08-17 16:10:56 -0400

    Added length tests.

---
+++ src/net/Qml.Net.Tests/Qml/ArrayTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using Xunit;
+
+namespace Qml.Net.Tests.Qml
+{
+    public class ArrayTests : BaseQmlTests<ArrayTests.ArrayTestsQml>
+    {
+        public class ArrayTestsQml
+        {
+            public virtual int[] GetArray()
+            {
+                return null;
+            }
+
+            public virtual void Test(object value)
+            {
+                
+            }
+        }
+
+        [Fact]
+        public void Can_get_length()
+        {
+            var array = new[] {3, 4, 6};
+            Mock.Setup(x => x.GetArray()).Returns(array);
+            Mock.Setup(x => x.Test(3));
+            
+            RunQmlTest(
+                "test",
+                @"
+                    var array = Net.toJsArray(test.getArray())
+                    test.test(array.length)
+                ");
+            
+            Mock.Verify(x => x.GetArray(), Times.Once);
+            Mock.Verify(x => x.Test(3), Times.Once);
+        }
+    }
+}
