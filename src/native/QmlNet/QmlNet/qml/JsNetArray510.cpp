93d97ecc94602cd8fd43b4337cb66d7f8b2ac05a
Paul Knopf <pauldotknopf@gmail.com>
2018-08-18 20:51:07 -0400

    Added push and pop.

--- src/native/QmlNet/QmlNet/qml/JsNetArray510.cpp
+++ src/native/QmlNet/QmlNet/qml/JsNetArray510.cpp
@@ -55,17 +55,91 @@ void NetArray::method_length(const BuiltinFunction *, Scope &scope, CallData *ca
 
 void NetArray::method_push(const BuiltinFunction *, Scope &scope, CallData *callData)
 {
+    ScopedObject instance(scope, callData->thisObject.toObject(scope.engine));
+    NetArray* netArray = instance->as<NetArray>();
+    Scoped<QV4::QObjectWrapper> wrapper(scope, netArray->d()->object);
+    NetValue* netValue = reinterpret_cast<NetValue*>(wrapper->d()->object());
+    QSharedPointer<NetTypeArrayFacade> arrayFacade = netValue->getNetReference()->getTypeInfo()->getArrayFacade();
 
+    if(arrayFacade == nullptr) {
+        THROW_GENERIC_ERROR("The wrapped object can't be treated as an array.");
+    }
+
+    if(arrayFacade->isFixed()) {
+        THROW_GENERIC_ERROR("Can't modify a fixed .NET list type.");
+    }
+
+    uint len = instance->getLength();
+    if(len == 0) {
+        RETURN_UNDEFINED();
+    }
+
+    for (int i = 0, ei = callData->argc; i < ei; ++i) {
+        QV4::ScopedValue valueScope(scope, callData->args[i]);
+        QJSValue valueJsValue(scope.engine, valueScope->asReturnedValue());
+        arrayFacade->push(netValue->getNetReference(), NetVariant::fromQJSValue(valueJsValue));
+    }
+
+    scope.result = Encode(instance->getLength());
 }
 
 void NetArray::method_pop(const BuiltinFunction *, Scope &scope, CallData *callData)
 {
+    ScopedObject instance(scope, callData->thisObject.toObject(scope.engine));
+    NetArray* netArray = instance->as<NetArray>();
+    Scoped<QV4::QObjectWrapper> wrapper(scope, netArray->d()->object);
+    NetValue* netValue = reinterpret_cast<NetValue*>(wrapper->d()->object());
+    QSharedPointer<NetTypeArrayFacade> arrayFacade = netValue->getNetReference()->getTypeInfo()->getArrayFacade();
 
+    if(arrayFacade == nullptr) {
+        THROW_GENERIC_ERROR("The wrapped object can't be treated as an array.");
+    }
+
+    if(arrayFacade->isFixed()) {
+        THROW_GENERIC_ERROR("Can't modify a fixed .NET list type.");
+    }
+
+    uint len = instance->getLength();
+    if(len == 0) {
+        RETURN_UNDEFINED();
+    }
+
+    QSharedPointer<NetVariant> result = arrayFacade->pop(netValue->getNetReference());
+    if(result == nullptr) {
+        scope.result = Encode::null();
+    }
+    QJSValue resultJsValue = result->toQJSValue(scope.engine->jsEngine());
+    scope.result = scope.engine->fromVariant(resultJsValue.toVariant());
 }
 
 void NetArray::method_forEach(const BuiltinFunction *, Scope &scope, CallData *callData)
 {
+    ScopedObject instance(scope, callData->thisObject.toObject(scope.engine));
+    if (!instance)
+        RETURN_UNDEFINED();
 
+    uint len = instance->getLength();
+
+    ScopedFunctionObject callback(scope, callData->argument(0));
+    if (!callback)
+        THROW_TYPE_ERROR();
+
+    ScopedCallData cData(scope, 3);
+    cData->thisObject = callData->argument(1);
+    cData->args[2] = instance;
+
+    ScopedValue v(scope);
+    for (uint k = 0; k < len; ++k) {
+        bool exists;
+        v = instance->getIndexed(k, &exists);
+        if (!exists)
+            continue;
+
+        cData->args[0] = v;
+        cData->args[1] = Primitive::fromDouble(k);
+        callback->call(scope, cData);
+    }
+    RETURN_UNDEFINED();
 }
 
 ReturnedValue NetArray::getIndexed(const Managed *m, uint index, bool *hasProperty)
@@ -90,5 +164,19 @@ ReturnedValue NetArray::getIndexed(const Managed *m, uint index, bool *hasProper
 
 bool NetArray::putIndexed(Managed *m, uint index, const Value &value)
 {
-    return false;
+    const NetArray *netArray = static_cast<const NetArray*>(m);
+    Scope scope(netArray->engine());
+    QV4::Scoped<QV4::QObjectWrapper> wrapper(scope, netArray->d()->object);
+    NetValue* netValue = reinterpret_cast<NetValue*>(wrapper->d()->object());
+    QSharedPointer<NetTypeArrayFacade> arrayFacade = netValue->getNetReference()->getTypeInfo()->getArrayFacade();
+
+    if(arrayFacade == nullptr) {
+        return scope.engine->throwError(QString::fromUtf8("The wrapped object can't be treated as an array."));
+    }
+
+    QV4::ScopedValue valueScope(scope, value);
+    QJSValue valueJsValue(scope.engine, valueScope->asReturnedValue());
+    arrayFacade->setIndexed(netValue->getNetReference(), index, NetVariant::fromQJSValue(valueJsValue));
+
+    return true;
 }
