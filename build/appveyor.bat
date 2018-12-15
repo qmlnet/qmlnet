08f35e0b7286955b002c77deb33f2f7659266cb2
Paul Knopf <pauldotknopf@gmail.com>
2018-12-14 21:43:26 -0500

    Fixing windows build

--- build/appveyor.bat
+++ build/appveyor.bat
@@ -1,5 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
-set PATH=%PATH%;C:\Qt\5.11.1\msvc2015_64\bin
+set PATH=%PATH%;C:\Qt\5.11.2\msvc2015_64\bin
 set PATH=%PATH%;C:\Qt\Tools\QtCreator\bin
 
 set PATH=%PATH%;%APPVEYOR_BUILD_FOLDER%\src\native\output
