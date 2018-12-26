41e41eebb0dfe040c585658c865f0f95a00fdbb4
Paul Knopf <pauldotknopf@gmail.com>
2018-12-25 23:03:48 -0500

    Don't need to cache anymore, download is quick.

--- build/travis.osx.sh
+++ build/travis.osx.sh
@@ -4,9 +4,7 @@ set -ex
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 QT_DIR=$SCRIPT_DIR/Qt
 
-if [ ! -e $QT_DIR\Qt\5.12.0 ]; then
-    wget -O- -q https://github.com/qmlnet/qmlnet/releases/download/ci/qt-5.12-osx-x64.tar.gz | tar xpz -C $QT_DIR
-fi
+wget -O- -q https://github.com/qmlnet/qmlnet/releases/download/ci/qt-5.12-osx-x64.tar.gz | tar xpz -C $QT_DIR
 
 export PATH=$PATH:$QT_DIR/Qt/5.12.0/clang_64/bin
 export DYLD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/src/native/output
