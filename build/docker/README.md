771dc954c0bd5a49131451b5b261593ff3f3e302
Paul Knopf <pauldotknopf@gmail.com>
2019-04-11 13:26:29 -0400

    The CI is now using the runtimes packaged at: https://github.com/qmlnet/qt-runtimes/releases/tag/releases

--- build/docker/README.md
+++
@@ -1,4 +0,0 @@
-Docker images used for different purposes.
-
-* linux-ci - Used for Linux CI (Travis)
-* windows-cross - Contains static qt5 libs for Windows (cross compilation).
--- build/docker/build-windows-cross.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-docker build $SCRIPT_DIR/windows-cross \
-    -t net-core-qml-windows-cross
