89d0bc3bee4a0084ff3785d39d9a74a262a5e870
Paul Knopf <pauldotknopf@gmail.com>
2019-04-11 20:57:35 -0400

    Removed the build steps that handles the Qt runtimes. They will be managed differently.

--- build/appveyor.bat
+++ build/appveyor.bat
@@ -3,5 +3,7 @@ set PATH=%PATH%;C:\qmlnet-qt\qt\bin
 set PATH=%PATH%;C:\qmlnet-qt\Tools\QtCreator\bin
 
 set PATH=%PATH%;%APPVEYOR_BUILD_FOLDER%\src\native\output
+set QT_PLUGIN_PATH=C:\qmlnet-qt\qt\plugins
+set QML2_IMPORT_PATH=C:\qmlnet-qt\qt\qml
 
 dotnet run -p ./build/scripts/Build.csproj -- ci
