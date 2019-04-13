b58be9718545a88f7c7a44f3f448547d1704eac6
Paul Knopf <pauldotknopf@gmail.com>
2019-04-13 12:21:16 -0400

    Support for loading windows/osx runtimes.

--- src/net/Qml.Net/Runtimes/RuntimeManager.cs
+++ src/net/Qml.Net/Runtimes/RuntimeManager.cs
@@ -139,42 +139,118 @@ namespace Qml.Net.Runtimes
                 throw new Exception($"The version of the runtime directory was {versionFile}, but expected {expectedVersion}");
             }
 
-            var pluginsDirectory = Path.Combine(directory, "qt", "plugins");
-            if (!Directory.Exists(pluginsDirectory))
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                throw new Exception($"Plugins directory didn't exist: {pluginsDirectory}");
-            }
-            Environment.SetEnvironmentVariable("QT_PLUGIN_PATH", pluginsDirectory);
-
-            var qmlDirectory = Path.Combine(directory, "qt", "qml");
-            if (!Directory.Exists(qmlDirectory))
-            {
-                throw new Exception($"QML directory didn't exist: {qmlDirectory}");
-            }
-            Environment.SetEnvironmentVariable("QML2_IMPORT_PATH", qmlDirectory);
-
-            var libDirectory = Path.Combine(directory, "qt", "lib");
-            if (!Directory.Exists(libDirectory))
-            {
-                throw new Exception($"The lib directory didn't exist: {libDirectory}");
-            }
-
-            var preloadPath = Path.Combine(libDirectory, "preload.txt");
-            if (!File.Exists(preloadPath))
-            {
-                throw new Exception($"The preload.txt file didn't exist: {preloadPath}");
-            }
-
-            var libsToPreload = File.ReadAllLines(preloadPath).Where(x => !string.IsNullOrEmpty(x))
-                .Select(x => Path.Combine(libDirectory, x))
-                .ToList();
-            var platformLoader = NetNativeLibLoader.Loader.PlatformLoaderBase.SelectPlatformLoader();
-            foreach (var libToPreload in libsToPreload)
-            {
-                var libHandler = platformLoader.LoadLibrary(libToPreload);
-                if (libHandler == IntPtr.Zero)
+                var pluginsDirectory = Path.Combine(directory, "qt", "plugins");
+                if (!Directory.Exists(pluginsDirectory))
                 {
-                    throw new Exception($"Unabled to preload library: {libToPreload}");
+                    throw new Exception($"Plugins directory didn't exist: {pluginsDirectory}");
+                }
+                Environment.SetEnvironmentVariable("QT_PLUGIN_PATH", pluginsDirectory);
+
+                var qmlDirectory = Path.Combine(directory, "qt", "qml");
+                if (!Directory.Exists(qmlDirectory))
+                {
+                    throw new Exception($"QML directory didn't exist: {qmlDirectory}");
+                }
+                Environment.SetEnvironmentVariable("QML2_IMPORT_PATH", qmlDirectory);
+
+                var libDirectory = Path.Combine(directory, "qt", "lib");
+                if (!Directory.Exists(libDirectory))
+                {
+                    throw new Exception($"The lib directory didn't exist: {libDirectory}");
+                }
+
+                var preloadPath = Path.Combine(libDirectory, "preload.txt");
+                if (!File.Exists(preloadPath))
+                {
+                    throw new Exception($"The preload.txt file didn't exist: {preloadPath}");
+                }
+
+                var libsToPreload = File.ReadAllLines(preloadPath).Where(x => !string.IsNullOrEmpty(x))
+                    .Select(x => Path.Combine(libDirectory, x))
+                    .ToList();
+                var platformLoader = NetNativeLibLoader.Loader.PlatformLoaderBase.SelectPlatformLoader();
+                foreach (var libToPreload in libsToPreload)
+                {
+                    var libHandler = platformLoader.LoadLibrary(libToPreload);
+                    if (libHandler == IntPtr.Zero)
+                    {
+                        throw new Exception($"Unabled to preload library: {libToPreload}");
+                    }
+                }
+            }
+            
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                var pluginsDirectory = Path.Combine(directory, "qt", "plugins");
+                if (!Directory.Exists(pluginsDirectory))
+                {
+                    throw new Exception($"Plugins directory didn't exist: {pluginsDirectory}");
+                }
+                Environment.SetEnvironmentVariable("QT_PLUGIN_PATH", pluginsDirectory);
+
+                var qmlDirectory = Path.Combine(directory, "qt", "qml");
+                if (!Directory.Exists(qmlDirectory))
+                {
+                    throw new Exception($"QML directory didn't exist: {qmlDirectory}");
+                }
+                Environment.SetEnvironmentVariable("QML2_IMPORT_PATH", qmlDirectory);
+
+                var libDirectory = Path.Combine(directory, "qt", "lib");
+                if (!Directory.Exists(libDirectory))
+                {
+                    throw new Exception($"The lib directory didn't exist: {libDirectory}");
+                }
+
+                var preloadPath = Path.Combine(libDirectory, "preload.txt");
+                if (!File.Exists(preloadPath))
+                {
+                    throw new Exception($"The preload.txt file didn't exist: {preloadPath}");
+                }
+
+                var libsToPreload = File.ReadAllLines(preloadPath).Where(x => !string.IsNullOrEmpty(x))
+                    .Select(x => Path.Combine(libDirectory, x))
+                    .ToList();
+                var platformLoader = NetNativeLibLoader.Loader.PlatformLoaderBase.SelectPlatformLoader();
+                foreach (var libToPreload in libsToPreload)
+                {
+                    var libHandler = platformLoader.LoadLibrary(libToPreload);
+                    if (libHandler == IntPtr.Zero)
+                    {
+                        throw new Exception($"Unabled to preload library: {libToPreload}");
+                    }
+                }
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var pluginsDirectory = Path.Combine(directory, "qt", "plugins");
+                if (!Directory.Exists(pluginsDirectory))
+                {
+                    throw new Exception($"Plugins directory didn't exist: {pluginsDirectory}");
+                }
+                Environment.SetEnvironmentVariable("QT_PLUGIN_PATH", pluginsDirectory);
+
+                var qmlDirectory = Path.Combine(directory, "qt", "qml");
+                if (!Directory.Exists(qmlDirectory))
+                {
+                    throw new Exception($"QML directory didn't exist: {qmlDirectory}");
+                }
+                Environment.SetEnvironmentVariable("QML2_IMPORT_PATH", qmlDirectory);
+
+                var binDirectory = Path.Combine(directory, "qt", "bin");
+                if (!Directory.Exists(binDirectory))
+                {
+                    throw new Exception($"The bin directory didn't exist: {binDirectory}");
+                }
+
+                Environment.SetEnvironmentVariable("PATH", $"{binDirectory};{Environment.GetEnvironmentVariable("PATH")}");
+
+                var preloadPath = Path.Combine(binDirectory, "preload.txt");
+                if (!File.Exists(preloadPath))
+                {
+                    throw new Exception($"The preload.txt file didn't exist: {preloadPath}");
                 }
             }
         }
