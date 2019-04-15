ad8c1bdd418c03bf125fab2cf9c04b0bc692bee9
Paul Knopf <pauldotknopf@gmail.com>
2019-04-14 23:01:40 -0400

    Installing mono in our Travis environments.

--- build/travis.linux.sh
+++ build/travis.linux.sh
@@ -3,10 +3,19 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 QT_DIR=$SCRIPT_DIR/Qt
 
+# Need to build Qt.
 sudo apt-get install -y libgl1-mesa-dev
 
+# Our Qt dev environment.
 mkdir -p $QT_DIR
 wget -O- -q https://github.com/qmlnet/qt-runtimes/releases/download/releases/qt-5.12.2-ad0689c-linux-x64-dev.tar.gz | tar xpz -C $QT_DIR
+
+# Needed to reference net472 in our native packages.
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+apt install apt-transport-https ca-certificates
+"deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+apt update
+apt-get install -y mono-complete
 
 export PATH=$QT_DIR/qt/bin:$PATH
 export LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/src/native/output:$QT_DIR/qt/lib
--- build/travis.osx.sh
+++ build/travis.osx.sh
@@ -4,8 +4,13 @@ set -e
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 QT_DIR=$SCRIPT_DIR/Qt
 
+# Our Qt dev environment.
 mkdir -p $QT_DIR
 wget -O- -q https://github.com/qmlnet/qt-runtimes/releases/download/releases/qt-5.12.2-ad0689c-osx-x64-dev.tar.gz | tar xpz -C $QT_DIR
+
+# Needed to reference net472 in our native packages.
+wget -O- -q https://download.mono-project.com/archive/5.20.1/macos-10-universal/MonoFramework-MDK-5.20.1.19.macos10.xamarin.universal.pkg > /tmp/mono.pkg
+installer -pkg /tmp/mono.pkg -target /
 
 export PATH=$QT_DIR/qt/bin:$PATH
 export DYLD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/src/native/output:$QT_DIR/qt/lib
