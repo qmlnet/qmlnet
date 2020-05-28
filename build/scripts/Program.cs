00f3ea00e129f591a5a8296736945d3cce985f64
Paul Knopf <pauldotknopf@gmail.com>
2020-05-28 15:21:24 -0400

    Fixing build.

--- build/scripts/Program.cs
+++ build/scripts/Program.cs
@@ -18,14 +18,14 @@ namespace Build
 {
     static class Program
     {
-        static Task Main(string[] args)
+        static void Main(string[] args)
         {
-            var options = ParseOptions<Options>(args);
+            var options = ParseOptions<RunnerOptions>(args);
             
             var nugetApiKey = Environment.GetEnvironmentVariable("PRIVATE_NUGET_KEY");
             var nugetSource = "https://www.myget.org/F/qmlnet/api/v3/index.json";
             var gitversion = GetGitVersion(ExpandPath("./"));
-            var commandBuildArgs = $"--configuration {options.Configuration} /p:Platform=\"Any CPU\"";
+            var commandBuildArgs = $"--configuration {options.Config} /p:Platform=\"Any CPU\"";
             var commandBuildArgsWithVersion = commandBuildArgs;
             if (!string.IsNullOrEmpty(gitversion.PreReleaseTag))
             {
@@ -139,15 +139,7 @@ namespace Build
 
             Target("ci", DependsOn("update-version", "build", "test", "deploy", "publish"));
 
-            return Run(options);
-        }
-
-        // ReSharper disable ClassNeverInstantiated.Local
-        class Options : RunnerOptions
-        // ReSharper restore ClassNeverInstantiated.Local
-        {
-            [PowerArgs.ArgShortcut("config"), PowerArgs.ArgDefaultValue("Release")]
-            public string Configuration { get; set; }
+            Execute(options);
         }
     }
 }
