f890c16808a80f3a90f2261f795643302ac8b964
Paul Knopf <pauldotknopf@gmail.com>
2020-08-01 22:52:37 -0400

    Updating Qt.

--- src/net/Qml.Net/QmlNetConfig.cs
+++ src/net/Qml.Net/QmlNetConfig.cs
@@ -4,7 +4,7 @@ namespace Qml.Net
 {
     public class QmlNetConfig
     {
-        public static string QtBuildVersion => "qt-5.12.2-ad0689c";
+        public static string QtBuildVersion => "qt-5.15.1-7fc8b10";
 
         public static bool ListenForExceptionsWhenInvokingTasks { get; set; }
 
